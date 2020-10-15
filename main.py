0066d7db284bc0104b7d42113ea1184f1609ac37
maximaxwell <48385374+maximaxwell@users.noreply.github.com>
2020-10-15 21:12:51 +0100

    Main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+
+City1 = input("Enter the name of your first city... ")
+City2 = input("Enter the name of your second city... ")
+print("Thankyou.")
+Out1 = City1.upper()
+Out2 = City2.upper()
+print(Out1[:4], "-", Out2[:4])
